df00655267cdb03d2b41be3b414e75e70439876c
Steve Brownlee <chortlehoort@gmail.com>
2019-01-21 00:02:10 -0600

    Initial commit

---
+++ scripts/ttt.js
@@ -0,0 +1,37 @@
+/*
+    Define directed graph of vertices and edges
+*/
+const adjacencyGraph = new Map()
+
+adjacencyGraph.set({s:1,e:2}, 0)
+adjacencyGraph.set({s:1,e:4}, 0)
+adjacencyGraph.set({s:1,e:5}, 0)
+adjacencyGraph.set({s:1,e:7}, 0)
+adjacencyGraph.set({s:1,e:9}, 0)
+adjacencyGraph.set({s:1,e:3}, 0)
+adjacencyGraph.set({s:2,e:3}, 0)
+adjacencyGraph.set({s:2,e:5}, 0)
+adjacencyGraph.set({s:2,e:8}, 0)
+adjacencyGraph.set({s:3,e:6}, 0)
+adjacencyGraph.set({s:3,e:7}, 0)
+adjacencyGraph.set({s:3,e:9}, 0)
+adjacencyGraph.set({s:4,e:5}, 0)
+adjacencyGraph.set({s:4,e:7}, 0)
+adjacencyGraph.set({s:5,e:6}, 0)
+adjacencyGraph.set({s:5,e:8}, 0)
+adjacencyGraph.set({s:5,e:9}, 0)
+adjacencyGraph.set({s:6,e:4}, 0)
+adjacencyGraph.set({s:6,e:9}, 0)
+adjacencyGraph.set({s:7,e:8}, 0)
+adjacencyGraph.set({s:7,e:9}, 0)
+adjacencyGraph.set({s:8,e:9}, 0)
+
+
+
+
+
+
+
+
+
+
